0f97202a6f22e8ad86c7db00ae326bd74d4da10e
Tuan le <leminhtuan48198@gmail.com>
2022-09-16 15:04:25 +0700

    add

---
+++ quanLyDiemThi.sql
@@ -0,0 +1,31 @@
+create database QuanLyDiemThi;
+use QuanLyDiemThi;
+create table HocSinh(
+MaHs varchar(20) primary key,
+tenHS varchar(50),
+NgaySinh datetime,
+Lop varchar(20),
+GT varchar(20)
+);
+create table MonHoc(
+MaMH varchar(20) primary key,
+TenMH varchar(50)
+);
+create table BangDiem(
+MaHS varchar(20),
+MaMH varchar(20),
+DiemThi int,
+NgayKT datetime,
+primary key (MaHS,MaMH),
+foreign key (MaHS) references HocSinh(MaHS),
+foreign key (MaMH) references MonHoc(MaMH)
+);
+create table GiaoVien(
+MaGV varchar(20) primary key,
+TenGV varchar(20),
+SDT Varchar(10)
+);
+alter table MonHoc 
+add MaGV varchar(20);
+alter table MonHoc add constraint FK_MaGV foreign key (MaGV) references GiaoVien(MaGV);
+
